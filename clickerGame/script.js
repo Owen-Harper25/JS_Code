5edfa5ae9026ec18914651ae6dd978c2520554a1
Owen-Harper25 <125112003+Owen-Harper25@users.noreply.github.com>
2023-04-17 11:45:52 -0700

    Addtional Work

--- clickerGame/script.js
+++ clickerGame/script.js
@@ -5,18 +5,20 @@ let income = 0
 let incomeCost = 50
 let t = 0
 let Bg = true
+let multiplier = 2
+
 
 function time(){
     requestAnimationFrame(time)
-    // console.log(t)
-    money += income / 60
-    document.getElementById("incomeD").innerText = ("Your bots are making $" + income + " per second");
+    money += multiplier * income / 60
+    document.getElementById("incomeD").innerText = ("Your bots are making $" + income * multiplier + " per second");
     document.getElementById("moneyCounter").innerText = ("Current Balance: $" + money.toFixed());
+    document.getElementById("multiCount").innerText = ("Current Multiplyer: " + multiplier);
 }
 time()
 
 function onButtonClick(){
-    money += value
+    money += value * multiplier
     document.getElementById("moneyCounter").innerText = ("Current Balance: $" + money.toFixed());
 }
 
@@ -27,7 +29,7 @@ function upgradeMoney(){
         upgradeCost += 5
         document.getElementById("moneyCounter").innerText = ("Current Balance: $" + money);
         document.getElementById("upgrades").innerText = ("Next upgrade price: $" + upgradeCost);
-        document.getElementById("clickValue").innerText = ("Your clicks are worth: $" + value);
+        document.getElementById("clickValue").innerText = ("Your clicks are worth: $" + value * multiplier);
         
     }
 }
@@ -41,10 +43,10 @@ function passIncome(){
     }
 }
 
-function bgChange(){
-    if (bg === true){
-        <body style="background-color:powderblue;"></body>
-        document.getElementById("moneyCounter").innerText = ("Blue");
+function onMultiClick(){
+    if (money >= incomeCost){
+        money -= incomeCost
+        multiplier += 1
+        console.log(multiplier)
     }
-
 }

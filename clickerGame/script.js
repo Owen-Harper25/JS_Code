5786ae7185761a053807513e5956f2df2afa8ef2
Owen-Harper25 <125112003+Owen-Harper25@users.noreply.github.com>
2023-04-19 18:44:22 -0700

    Attempting Table

--- clickerGame/script.js
+++ clickerGame/script.js
@@ -1,10 +1,12 @@
 let money = 0
-let value = 1
+let click = 1
 let upgradeCost = 10
 let income = 0
-let incomeCost = 50
-let t = 0
-let multiplier = 50
+let passPencil = 50
+let passNotebook = 100
+let passTextbook = 250
+let timeA = 0
+let multiplier = 1
 let speed = 1
 let mechPencilCost = 500
 let backPackCost = 200
@@ -12,46 +14,73 @@ let mechPencil = false
 let backPack = false
 let clickPower = 1
 let totalMoney = 0
+let n = 0
+let p = 0
+let t = 0
+
+
+function onButtonClick(){
+    money += click * multiplier * clickPower
+    totalMoney += click * multiplier * clickPower
+    document.getElementById("moneyCounter").innerText = ("Current knowledge: " + money.toFixed() + "kp");
+    document.getElementById("moneyTotal").innerText = ("Total knowledge: " + totalMoney.toFixed() + "kp");
+
+}
+
+function upgradeClick(){
+    if (money >= upgradeCost){
+        money -= upgradeCost
+        click += 1
+        upgradeCost += 5
+        document.getElementById("moneyCounter").innerText = ("Current knowledge: " + money.toFixed()+ "kp");
+        document.getElementById("upgrades").innerText = ("Next upgrade price: $" + upgradeCost);
+        document.getElementById("pageValue").innerText = ("Your pages are worth: " + click * multiplier * clickPower + "kp each");
+        
+    }
+}
+
+function upgradePencil(){
+    if (money >= passPencil){
+        money -= passPencil
+        income += 0.1
+        p +=1
+        passPencil += 15 + 3 * p
+        document.getElementById("incomePrice").innerText = ("Next pencil is " + passPencil + "kp");
+    }
+}
+
+function upgradeNotebook(){
+    if (money >= passNotebook){
+        money -= passNotebook
+        income += 1
+        n +=1
+        passNotebook += 15 + 5 * n
+        document.getElementById("incomePrice").innerText = ("Next pencil is " + passNotebook + "kp");
+    }
+}
+
+function upgradeTextbook(){
+    if (money >= passTextbook){
+        money -= passTextbook
+        income += 1
+        n +=1
+        passTextbook += 15 + 5 * n
+        document.getElementById("incomePrice").innerText = ("Next pencil is " + incomeCost + "kp");
+    }
+}
+
 
 function time(){
     requestAnimationFrame(time)
     money += multiplier * speed * income / 60
     totalMoney += multiplier * speed * income / 60
-    document.getElementById("incomeD").innerText = ("You are making " + income * multiplier * speed + "kp per second");
+    document.getElementById("incomeD").innerText = ("You are making " + (income * multiplier * speed).toFixed(1) + "kp per second");
     document.getElementById("moneyCounter").innerText = ("Current knowledge: " + money.toFixed() + "kp");
     document.getElementById("multiCount").innerText = ("Current multiplyer: " + multiplier);
     document.getElementById("moneyTotal").innerText = ("Total knowledge: " + totalMoney.toFixed() + "kp");
 }
 time()
 
-function onButtonClick(){
-    money += value * multiplier * clickPower
-    totalMoney += value * multiplier * clickPower
-    document.getElementById("moneyCounter").innerText = ("Current knowledge: " + money.toFixed() + "kp");
-    document.getElementById("moneyTotal").innerText = ("Total knowledge: " + totalMoney.toFixed() + "kp");
-
-}
-
-function upgradeMoney(){
-    if (money >= upgradeCost){
-        money -= upgradeCost
-        value += 1
-        upgradeCost += 5
-        document.getElementById("moneyCounter").innerText = ("Current knowledge: " + money.toFixed()+ "kp");
-        document.getElementById("upgrades").innerText = ("Next upgrade price: $" + upgradeCost);
-        document.getElementById("pageValue").innerText = ("Your pages are worth: " + value * multiplier * clickPower + "kp each");
-        
-    }
-}
-
-function passIncome(){
-    if (money >= incomeCost){
-        money -= incomeCost
-        income += 1
-        incomeCost += 50
-        document.getElementById("incomePrice").innerText = ("Next pencil is " + incomeCost + "kp");
-    }
-}
 
 function onMechUpgrade(){
     if (money >= mechPencilCost && mechPencil === false){
@@ -69,7 +98,7 @@ function onBagUpgrade(){
         backPack = true
         money -= backPackCost
         clickPower += 1
-        document.getElementById("pageValue").innerText = ("Your pages are worth: " + value * multiplier * clickPower + "kp each");
+        document.getElementById("pageValue").innerText = ("Your pages are worth: " + click * multiplier * clickPower + "kp each");
         document.getElementById("bagA").style.display="none";
         document.getElementById("bagB").style.display="none";
     }

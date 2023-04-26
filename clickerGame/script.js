d95042c510e947db041bc0985dc120cdb0296dee
Owen-Harper25 <125112003+Owen-Harper25@users.noreply.github.com>
2023-04-26 12:21:12 -0700

    Upgrade Upgrade

--- clickerGame/script.js
+++ clickerGame/script.js
@@ -84,7 +84,7 @@ function time(){
     document.getElementById("moneyCounter").innerText = ("Current knowledge: " + money.toFixed() + "kp");
     document.getElementById("moneyTotal").innerText = ("Total knowledge: " + totalMoney.toFixed() + "kp");
     document.getElementById("multiCount").innerText = ("Current multiplyer: " + multiplier);
-    document.getElementById("pencilID").innerText = (`Each Pencil increases your kps by 0.1 \n You have ${p} Pencils making ` + (pencilValue * p).toFixed(1) + "kp per second.");
+    document.getElementById("pencilID").innerText = (`Each Pencil increases your kps by ${pencilValue} \n You have ${p} Pencils making ` + (pencilValue * p).toFixed(1) + "kp per second.");
     document.getElementById("noteID").innerText = (`Each Notebook increases your kps by 1 \n You have ${n} Notebooks making ${notebookValue * n}kp per second`);
     document.getElementById("ruleID").innerText = (`Each Ruler increases your kps by 3 \n You have ${r} Rulers making ${rulerValue * r}kp per second`);
     document.getElementById("textbookID").innerText = (`Each Textbook increases your kps by 5 \n You have ${t} Textbooks making ${textbookValue * t}kp per second`);
@@ -102,18 +102,6 @@ function time(){
 }
 time()
 
-
-function onMechUpgrade(){
-    if (money >= mechPencilCost && mechPencil === false){
-        mechPencil = true
-        money -= mechPencilCost
-        speed += 1
-        document.getElementById("incomeD").innerText = ("You are making " + income * multiplier * speed + "kp per second");
-        document.getElementById("mechA").style.display="none";
-        document.getElementById("mechB").style.display="none";
-    }
-}
-
 function onBagUpgrade(){
     if (money >= backPackCost && backPack === false){
         backPack = true
@@ -121,5 +109,15 @@ function onBagUpgrade(){
         clickPower += 1
         document.getElementById("bagA").style.display="none";
         document.getElementById("bagB").style.display="none";
+    }
+}
+
+function onMechUpgrade(){
+    if (money >= mechPencilCost && mechPencil === false){
+        mechPencil = true
+        money -= mechPencilCost
+        pencilValue *= 2
+        document.getElementById("mechA").style.display="none";
+        document.getElementById("mechB").style.display="none";
     }
 }
